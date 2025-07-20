69e592966389a6843f3d967c61867a35dcbe0f50
Pedro H G Migacz <pedromigacz@gmail.com>
2025-07-20 17:35:37 -0300

    feat: vecs1

--- exercises/05_vecs/vecs1.rs
+++ exercises/05_vecs/vecs1.rs
@@ -4,6 +4,8 @@ fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     // TODO: Create a vector called `v` which contains the exact same elements as in the array `a`.
     // Use the vector macro.
     // let v = ???;
+    // Needs the vec macro because the return type requires a vec and not a fixed size array
+    let v = vec![10, 20, 30, 40];
 
     (a, v)
 }
